46ddfd51cabfde15112f58857c6f359028daf4b8
Douglas Camata de Araujo <105362566+douglas-camata@users.noreply.github.com>
2023-06-17 14:27:31 -0300

    Atualizacao

--- js/script.js
+++ js/script.js
@@ -5,7 +5,15 @@ var pontuacao = 0
 
 function carregarPergunta(){
     var iSorteada = parseInt(Math.random() * listaPerguntas.length)
-       
+    
+    respCorreta = listaPerguntas[iSorteada].correta
+    document.getElementById('pergunta').innerHTML = listaPerguntas[iSorteada].pergunta
+    document.getElementById('imagem').src = listaPerguntas[iSorteada].imagem
+    document.getElementById('resp1').innerHTML = listaPerguntas[iSorteada].resp1
+    document.getElementById('resp2').innerHTML = listaPerguntas[iSorteada].resp2
+    document.getElementById('resp3').innerHTML = listaPerguntas[iSorteada].resp3
+    document.getElementById('resp4').innerHTML = listaPerguntas[iSorteada].resp4
+    
 }
 carregarPergunta()
 
@@ -24,3 +32,4 @@ function respondeu(resposta){
 
 
 }
+
